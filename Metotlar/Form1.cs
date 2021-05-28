ddbab8f93181c828fe52aa916a4185c4041a2a9b
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-05-28 11:20:45 +0300

    geriye değer döndürmeyen methodu gördük burada

--- Metotlar/Form1.cs
+++ Metotlar/Form1.cs
@@ -16,5 +16,34 @@ namespace Metotlar
         {
             InitializeComponent();
         }
+
+        public void temizle()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox1.Focus(); //imcleci buradan başlat
+        }
+
+        void renklendir()
+        {
+            textBox1.BackColor = Color.Red;
+            textBox2.BackColor = Color.Green;
+            textBox3.BackColor = Color.Blue;
+            textBox4.BackColor = Color.Yellow;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            temizle();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            renklendir();
+        }
     }
 }
